27f8e717cfec7b2409144b2c02e75e5fdd0ec497
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-10-01 20:13:09 -0300

    add composition-api support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-	buildModules: ['@nuxt/typescript-build'],
+	buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api/module'],
 	components: true,
 	srcDir: 'src',
 	target: 'static',
