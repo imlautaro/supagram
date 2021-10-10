8ba634b128210ec6cbacc69ed68d58419d026dbe
Lautaro Pereyra <hi@imlautaro.com>
2021-10-10 11:26:06 -0300

    add branding colors

--- windi.config.ts
+++ windi.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'windicss/helpers'
+import colors from 'windicss/colors'
 import native from 'windi-native'
 
 const config = defineConfig({
 	plugins: [native],
+	theme: {
+		extend: {
+			colors: {
+				gray: colors.blueGray,
+				primary: colors.rose,
+			},
+		},
+	},
 })
 
 export default config
