f9c54698b74c69d402279083e69df49c29ae7fbf
Lautaro Pereyra <hi@imlautaro.com>
2021-10-10 13:17:20 -0300

    add text-field basic component

---
+++ src/components/ui/TextField.vue
@@ -0,0 +1,17 @@
+<template>
+	<input
+		class="bg-gray-100 px-5 py-3 rounded-2xl"
+		@input="(event) => $emit('input', event.currentTarget.value)"
+		:value="value"
+	/>
+</template>
+
+<script>
+export default {
+	props: ['value'],
+	model: {
+		prop: 'value',
+		event: 'input',
+	},
+}
+</script>
